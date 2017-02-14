bac08989146215edcbbc1474a0b64f351464b192
Greg Wienecke <gregwienecke@gmail.com>
2017-02-13 22:51:02 -0600

    Added back button to shop component. Connected firebase

--- app/shop.component.ts
+++ app/shop.component.ts
@@ -3,8 +3,57 @@ import { Component }	from '@angular/core';
 @Component({
 	selector: 'shop',
 	template: `
-		<h1>Test</h1>
+		<div class="header">
+			<h1>Shop</h1>
+		</div>
+
+		<div class="shop">
+			<button>Add To Cart</button><br><br>
+			<a routerLink="/gallery"><button>Back</button></a>
+		</div>
+
+		<footer></footer>
 	`,
+	styles: [`
+		.header {
+			height: 285px;
+			width: 90%;
+			//background-color: #ccc;
+			background-image: url("/images/sevenposter.jpg");
+			background-position: -450px -60px;
+			margin: auto;
+			text-align: center;
+		}
+
+		h1 {
+			background-color: salmon;
+			padding: 10px 0 10px 0;
+			color: #ddd;
+			width: 140px;
+			margin: auto;
+		}
+
+		.shop {
+			width: 90%;
+			margin: auto;
+			padding-top: 40px;
+		}
+
+		button {
+			background-color: black;
+			color: salmon;
+			border: none;
+			padding: 5px 10px 5px 10px;
+		}
+
+		button:hover {
+			background-color: #222;
+		}
+
+		footer {
+			height: 100px;
+		}
+	`]
 })
 export class ShopComponent {
 
